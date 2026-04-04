1e1997b70f608a5cade22ff8343d56484757e291
Cabinet <apotatohacker@gmail.com>
2026-04-03 22:36:48 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,28 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(), 
+      tailwindcss(),
+      VitePWA({
+        strategies: 'injectManifest',
+        srcDir: 'src',
+        filename: 'sw.js',
+        injectManifest: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,json,woff,woff2}']
+        },
+        manifest: false,
+        devOptions: {
+          enabled: true,
+          type: 'module',
+        }
+      })
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
