23e70683604a2a32936fbed6303cef3abd210217
Cabinet <apotatohacker@gmail.com>
2026-04-04 23:36:12 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ async function startServer() {
   // Middleware for basic security headers
   app.use((req, res, next) => {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-    res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
     res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
     next();
   });
