730646638da60240a1e97aa393a1c9a1cf3943f7
Cabinet <apotatohacker@gmail.com>
2026-04-04 23:36:27 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig(({mode}) => {
       hmr: false,
       headers: {
         "Cross-Origin-Opener-Policy": "same-origin",
-        "Cross-Origin-Embedder-Policy": "credentialless",
+        "Cross-Origin-Embedder-Policy": "require-corp",
       },
     },
   };
